3593c28626a3d4e5a08925cdce51f0961a5b5141
Mubin Saeed <mubin.saeed@techlogix.com>
2024-06-25 16:58:02 +0500

    Notification module added

---
+++ notification/src/main/java/com/mubin/Main.java
@@ -0,0 +1,11 @@
+package com.mubin;
+
+
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
